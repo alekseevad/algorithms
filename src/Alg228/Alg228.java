a084ba7741b188c6f8450c3c74528fbffe0f6908
Alekseev.A <Alekseev.Ar@gitlab.raidix.com>
2023-03-25 17:34:54 +0300

    init

---
+++ src/Alg228/Alg228.java
@@ -0,0 +1,8 @@
+package Alg228;
+
+public class Alg228 {
+    public static void main(String[] args) {
+        int num[] = {0, 1, 2, 4, 7, 8, 9, 11};
+        System.out.println(Solution.summaryRanges(num));
+    }
+}
